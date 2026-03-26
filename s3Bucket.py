635063047f4c63fa3c974d9241b130b159c2cd79
MitchellGerdisch <mitch@pulumi.com>
2026-03-26 14:09:44 -0500

    010

--- s3Bucket.py
+++ s3Bucket.py
@@ -354,7 +354,7 @@ class S3Bucket(pulumi.ComponentResource):
         self.bucket_id = bucket.id
         self.bucket_arn = bucket.arn
         # self.bucket_domain_name = bucket.bucket_domain_name
-        self.bucket_domain_name = "Latest change"
+        self.bucket_domain_name = "this is version 0.1.0 code"
         self.bucket_regional_domain_name = bucket.bucket_regional_domain_name
         self.log_bucket_id = log_bucket.id
         self.log_bucket_arn = log_bucket.arn
